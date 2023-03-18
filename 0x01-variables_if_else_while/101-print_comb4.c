9665d87e0cda786344b769a708c30549366a5486
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-18 11:25:50 -0700

    sd

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+/**
+* main - main entry.
+*
+* Return: Always 0
+*/
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/* The function generates  random integer between -RAND_MAX/2 and RAND_MAX/2.*/
+int main(void)
+{
+int i, j, k;
+for (i = 48; i <= 57; ++i)
+{
+for (j = i + 1; j <= 57; ++j)
+{
+for (k = j + 1; k <= 57; ++k)
+{
+putchar(i);
+putchar(j);
+if (i == 55 && j == 56 && k== 57)
+{
+putchar('\n');
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
