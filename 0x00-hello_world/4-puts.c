fcd08b2eb3b7d15a047d4ca0ba0cdeba36c66e4f
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-18 06:36:19 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,7 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(){
-
-puts("Programming is like building a multilingual puzzle\n");
+int main(void){
+puts("\"Programming is like building a multilingual puzzle\n");
 return 0;
 }
