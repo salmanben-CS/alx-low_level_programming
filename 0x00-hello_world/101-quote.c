da537035eba79a9ec2066821e243ce2e9c203dcc
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-18 07:25:12 -0700

    sd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<string.h>
 /**
 * main - main entry.
 *
@@ -10,11 +10,13 @@
 /* betty style doc for function main goes there */
 int main()
 {
-char mot[200]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-int i=0;
-while(mot[i]!='\0')
+char mot[200] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+int i = 0;
+while(i < strlen(mot) )
 {
 putchar(mot[i]);
+if(i = strlen(mot) - 1)
+putchar('\n');
 i++;
 }
 
