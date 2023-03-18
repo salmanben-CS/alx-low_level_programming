69e6cf605a6984cd7da5e9ab3797a8341ef2a426
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-18 07:17:42 -0700

    ds

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+* main - main entry.
+*
+* Return: Always 0
+*/
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main()
+{
+char mot[200]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+int i=0;
+while(mot[i]!='\0')
+{
+putchar(mot[i]);
+i++;
+}
+
+return (1);
+}
