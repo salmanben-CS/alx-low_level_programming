0d4d44705d1f50d4dffb9afe8c96d9b4058b1767
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-18 10:19:16 -0700

    Ds

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - main entry.
+*
+* Return: Always 0
+*/
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/* The function generates  random integer between -RAND_MAX/2 and RAND_MAX/2.*/
+int main(void)
+{
+for (i = 0; i <= 9; ++i)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
