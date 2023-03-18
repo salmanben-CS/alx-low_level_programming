9cf6de4a4e76526a451a7e4f409e54aeffe81164
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-18 06:33:17 -0700

    ds

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include<stdio.h>
+/**
+* main - main entry.
+*
+* Return: Always 0
+*/
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(){
 
 puts("Programming is like building a multilingual puzzle\n");
