3134b95f9397daf0123268a03073fc86b235f237
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-18 10:13:29 -0700

    ds

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - main entry.
+*
+* Return: Always 0
+*/
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/* The function generates  random integer between -RAND_MAX/2 and RAND_MAX/2.*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+{
+if(c == 'q' || c == 'e')
+continue;
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
