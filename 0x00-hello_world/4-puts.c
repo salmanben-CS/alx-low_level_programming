137a17465d9eb0577001588bd6dec492b058fe4d
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-18 06:47:48 -0700

    dd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,6 +9,6 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
