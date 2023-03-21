3f10b1c7ec724aae0edad211a1be0301cbb6dc16
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-21 10:23:04 -0700

    ds

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "main.h"
+/**
+* add: add two numbers
+* Return: 1 if is > 0 and 0 if is == 0
+* and -1 if is <0
+*/
+int add(int a, int b)
+{
+int sum = a + b;
+return (sum);
+}
