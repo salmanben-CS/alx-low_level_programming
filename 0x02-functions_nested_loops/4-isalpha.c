4bd679689f98619b33ab377905801bc2822df28b
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-21 11:15:08 -0700

    ds

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 #include "main.h"
 /**
-* islower - check if the letter is lower or no.
+* _isalpha  - check if the letter is lower or no.
 *@c:letter taht will be checked
 * Return: 1 if is lower. and 0 if no
 */
 int _isalpha(int c)
 {
-if ((c >= 97 && c <= 122) || (c >= 65 && c<= 90))
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 return (1);
 else
 return (0);
-}	
+}
