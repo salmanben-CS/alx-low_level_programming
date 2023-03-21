f4640c0eb4c47d8186a2bc917d8c739932187e2d
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-21 12:27:36 -0700

    d

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,13 +12,13 @@ for (row = 0; row <= 9; row++)
 for (col = 0; col <= 9; col++)
 {
 printf("%2d", row * col);
-if (col < 9 && col != 81)
+if (col < 9)
 {
 printf(", ");
 }
 else
 {
- printf("\n");
+printf("\n");
 }
 }
 }
