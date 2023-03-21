fe7a1a71ac54243086615b7fa1c982f7a22be6d4
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-21 07:34:23 -0700

    ds

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+* main - main entry.
+*
+* Return: Always 0
+*/
+int main(){
+char mot[20] = "_putchar";
+int i = 0;
+while (mot[i] != '\0')
+{
+putchar(mot[i]);
+i++;
+} 
+putchar('\n');
+return (0);
+}
