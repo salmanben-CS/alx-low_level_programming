926862cb1af26eb7332b0c6e49f61c317324c896
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-21 09:22:48 -0700

    ds

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
 * _islower - check if the letter is lower or no.
-*
+* c :letter passed as arg
 * Return: 1 if c is lower. and 0 if no
 */
 int _islower(int c)
@@ -11,4 +11,4 @@ if (c >= 97 && c <= 122)
 return (1);
 else
 return (0);
-}	
+}
