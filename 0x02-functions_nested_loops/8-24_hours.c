9dc142d31c72f4cf62d4865f98e489da82d00999
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-21 12:39:43 -0700

    sd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,5 +6,23 @@
 */
 void jack_bauer(void)
 {
-
+int i, j;
+for (i = 0; i < 24; i++)
+{
+for (j = 0; j < 60; j++)
+{
+if (i < 10)
+{
+printf("0");
+}
+printf("%d",i);
+putchar(':');
+if (j < 10)
+{
+printf("0");
+}
+printf("%d",j);
+putchar('\n');
+}
+}
 }
