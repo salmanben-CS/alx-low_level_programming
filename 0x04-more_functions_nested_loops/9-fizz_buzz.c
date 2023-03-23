3984b9315273da69c96e10f16ddd261351aa5a2a
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-23 10:25:01 -0700

    ds

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ for (i = 1; i <= 100; i++)
 {
 if ((i % 3 == 0) && (i % 5 == 0))
 printf("FizzBuzz");
-else if (i % 3 == 0)
+else if ((i % 3 == 0) && !(i % 5 == 0))
 printf("Fizz");
 else if (i % 5 == 0)
 printf("Buzz");
