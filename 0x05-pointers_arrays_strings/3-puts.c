18f3ea1847190785781ebe5b003aaf3e8b2c70e2
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-27 07:30:37 -0700

    sd

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _puts -prints string to the stdout
+*@s: is a pointer points on the string that will be printed
+* Return:void
+*/
+void _puts(char *str)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+_putchar(s[i]);
+}
