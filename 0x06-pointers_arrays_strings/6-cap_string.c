b1f02085748af71601ff46d82d51845b76669b04
salmanben-CS <salmanbenomar250@gmail.com>
2023-03-30 16:32:16 -0700

    sd

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string  -   capitalizes all words
+ * @s:pointer point on a string
+ * Return:pointer point on the new string
+ */
+char *cap_string(char *s)
+{
+int i, ascii, ascii_before;
+ascii = s[0];
+if (ascii >= 97 && ascii <= 120)
+{
+ascii -= 32;
+s[0] = ascii;
+}
+for (i = 1; s[i] != '\0'; i++)
+{
+ascii = s[i];
+ascii_before = s[i - 1];
+if (ascii >= 97 && ((s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n')
+||
+(ascii_before < 65) || (ascii_before > 120)))
+{
+ascii -= 32;
+s[i] = ascii;
+}
+}
+return (s);
+}
