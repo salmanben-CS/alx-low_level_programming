7d01f53a29df99ab245a73fb65d0da6e02875952
salmanben-CS <salmanbenomar250@gmail.com>
2023-05-02 03:25:05 -0700

    fix error'

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,16 +8,11 @@
 size_t print_listint(const listint_t *h)
 {
 size_t count = 0;
-if (h == NULL)
-printf("(nil)\n");
-else
-{
 while (h != NULL)
 {
 printf("%d\n", h->n);
 count++;
 h = h->next;
-}
 }
 return (count);
 }
