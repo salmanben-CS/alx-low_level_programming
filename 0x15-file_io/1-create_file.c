5be263a50bbcf0f28d119c3ba8bc2c9e6cf7019a
salmanben-CS <salmanbenomar250@gmail.com>
2023-07-10 17:30:10 -0700

    Create 1-create_file.c file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
-/*
- * 
- * creating file's name
- * 
- * Always return: 1 on success, -1 on failure
- *
+/**
+ * create_file - Create a file and write to it
+ * @filename: the name of file
+ * @text_content: the text will be written to the created file
+ * Return: 1 if success and -1 if not
  */
 
 int create_file(const char *filename, char *text_content)
